d867725ce159235fe757316410c18bfb8ea41676
PopekD <dawid.popek01@gmail.com>
2023-05-23 20:17:51 +0300

    Add dist

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ function Sidebar ()  {
     return ( 
         <>
             <Menu className={"Sidebar"} >
-                <a href='/'><img src="../pictures/logo.png" className='logo'></img></a>
+                <a href='/'><img src="pictures/logo.png" className='logo'></img></a>
                 <div className='links' style={{position: "absolute", bottom: "0", display: "flex", flexDirection:"column"}}>
                     <a href='/Strona/aboutMe'>About Me</a>
                     <a href='https://www.instagram.com/hosehoundart/' target="_blank" rel="noreferrer">Instagram</a>
